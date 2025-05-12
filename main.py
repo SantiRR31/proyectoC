d31e23aee956c932111bb5420ca148284971c0e1
SantiRR31 <santiagorodriguezrojo31@gmail.com>
2025-05-12 13:42:03 -0600

    third

--- main.py
+++ main.py
@@ -11,10 +11,16 @@ import customtkinter as ctk
 ctk.set_appearance_mode("light")  # Modes: "system" (default), "light", "dark"
 ctk.set_default_color_theme("blue")  # Themes: "blue" (default), "green", "dark-blue"
 
-app = ctk.CTk()  
-app.geometry("400x300")
-app.title("Programa")
+icon_path = ("assets/cecati-122.ico")
 
+app = ctk.CTk()  
+app.geometry("720x480")
+app.title("Programa")
+app.iconbitmap(icon_path)
+
+# --- INTERFAZ PRINCIPAL ---
+tittle = ctk.CTkLabel(app, text="Sistema de Control de Asistencia", font=("Arial", 24))
+tittle.pack(pady=20)
 
 
 app.mainloop()
