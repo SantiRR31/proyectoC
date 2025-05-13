42e959fc7e2e0cebd14e499d434a519a4737bde6
ShaguittoScot <arields.ti23@utsjr.edu.mx>
2025-05-13 11:47:24 -0600

    Diseño base

--- main.py
+++ main.py
@@ -1,39 +1,4 @@
-# Interfaz principal al lanzar el programa
-# Este archivo contiene la interfaz gráfica del programa
-# Importar las librerías necesarias
-# Importar las librerías necesarias
-# Importar las librerías necesarias
-# Importar las librerías necesarias
-# Importar las librerías necesarias        
-import tkinter as tk
-import customtkinter as ctk
+from ui.home import lanzar_ventana_principal
 
-ctk.set_appearance_mode("light")  # Modes: "system" (default), "light", "dark"
-ctk.set_default_color_theme("blue")  # Themes: "blue" (default), "green", "dark-blue"
-
-icon_path = ("assets/cecati-122.ico")
-
-app = ctk.CTk()  
-app.geometry("720x480")
-app.title("Programa")
-app.iconbitmap(icon_path)
-
-# --- INTERFAZ PRINCIPAL ---
-tittle = ctk.CTkLabel(app, text="Sistema de control de ingresos", font=("Arial", 24))
-tittle.pack(pady=20)
-
-
-frame1 = ctk.CTkFrame(app, fg_color="transparent")
-frame1.pack(pady=20)
-
-partida = ctk.CTkEntry(frame1, placeholder_text="Partida")
-partida.grid(row=0, column=0, padx=10)
-resultado = ctk.CTkEntry(frame1, placeholder_text="Resultado")
-resultado.grid(row=0, column=1, padx=10)
-btnOK = ctk.CTkButton(frame1, text="SI")
-btnOK.grid(row=0, column=2, padx=10)
-
-
-
-
-app.mainloop()
+if __name__ == "__main__":
+    lanzar_ventana_principal()
---
+++ styles/styles.py
@@ -0,0 +1,33 @@
+# estilos.py
+
+estilo_btn = {
+        "corner_radius": 10,
+        "fg_color": "#007acc",
+        "hover_color": "#005f99",
+        "text_color": "white",
+        "font": ("Arial", 14),
+        "height": 35,
+        "width": 160
+    }
+
+boton_primario = {
+    "corner_radius": 10,
+    "fg_color": "#007acc",
+    "hover_color": "#005f99",
+    "text_color": "white",
+    "font": ("Arial", 14),
+    "height": 35,
+    "width": 160
+}
+
+boton_secundario = {
+    "corner_radius": 10,
+    "fg_color": "#d9d9d9",
+    "hover_color": "#c0c0c0",
+    "text_color": "black",
+    "font": ("Arial", 14),
+    "height": 35,
+    "width": 160
+}
+
+navbar_color = "#e6e9f0"
